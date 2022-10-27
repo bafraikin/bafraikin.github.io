88d9590d11c60bf07c38daac74e16bcb67059416
Baptiste Fraikin <fraikin.baptiste@gmail.com>
2022-10-27 14:56:42 +0200

    isit fixed ?

--- dist/pdfjsWorker.js
+++ dist/pdfjsWorker.js
@@ -1,1 +1,14 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{123:function(n,o,e){n.exports=function(){return new Worker(e.p+"index.worker.js")}}}]);
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([["pdfjsWorker"],{
+
+/***/ "./node_modules/pdfjs-dist/build/pdf.worker.js":
+/*!*****************************************************!*\
+  !*** ./node_modules/pdfjs-dist/build/pdf.worker.js ***!
+  \*****************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+eval("module.exports = function() {\n  return new Worker(__webpack_require__.p + \"index.worker.js\");\n};\n\n//# sourceURL=webpack:///./node_modules/pdfjs-dist/build/pdf.worker.js?");
+
+/***/ })
+
+}]);
